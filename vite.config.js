9f083d22c78a67cdc3133ba0f97bde79138c074f
zhengjia wang <jajawng@163.com>
2025-01-08 22:18:04 +0900

    add CA cert

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
+import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +16,11 @@ export default defineConfig({
   },
   envPrefix: "APP_",
   server: {
+    https: {
+      // key: fs.readFileSync(path.resolve(__dirname, ".cert/myCAkey.pem")),
+      key: fs.readFileSync(path.resolve(__dirname, ".cert/myCAkey-decrypted.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, ".cert/server.crt")),
+    },
     host: "0.0.0.0", // Allow access from any device on the local network
     port: 3000, // Or your desired port
   },
